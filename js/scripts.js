85b573a6db0f0b7090eae60a5ed37c5bc03c85e4
Julius Bade and Mark Mangahas <julius@>
2018-08-08 11:01:20 -0700

    finsihed

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,24 @@
 $(document).ready(function() {
-  $("button#hello").click(function() {
-    console.log("IT WORKS");
-    $("ul#user").prepend("<li>Hello!</li>");
-    $("ul#webpage").prepend("<li>Why hello there!</li>");
+  $("#blanks").submit(function(event)  {
+  console.log ("IT WORKS");
+  var person1Input = $("input#person1").val();
+  var person2Input = $("input#person2").val();
+  var animalInput = $("input#animal").val();
+  var exclamationInput = $("input#exclamation").val();
+  var verbInput = $("input#verb").val();
+  var nounInput = $("input#noun").val();
 
-    $("ul#user").children("li").first().click(function() {
-    $(this).remove();
-   });
-  $("ul#webpage").children("li").first().click(function() {
-    $(this).remove();
-   });
- });
 
-  $("button#goodbye").click(function() {
-    $("ul#user").prepend("<li>Goodbye!</li>");
-    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
 
-    $("ul#user").children("li").first().click(function() {
-    $(this).remove();
-   });
-    $("ul#webpage").children("li").first().click(function() {
-    $(this).remove();
-   });
- });
+    $("#story").show();
 
-  $("button#stop").click(function() {
-    $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    event.preventDefault();
 
-   });
- });
-
- $(document).ready(function() {
-   $("input").first().click(function() {
-     alert("Meeeeeeeeoooooooow!");
-     var num = Math.random()%1000;
-     $("input").first().before('<img src="https://loremflickr.com/350/250/cat?random=' + num + '"><br><br>');
-   });
-   $("input").eq(1).click(function() {
-     alert("Bark!");
-     var num = Math.random()%1000;
-     $("input").eq(1).before('<img src="https://loremflickr.com/350/250/dog?random=' + num + '"><br><br>');
-   });
+  });
 });
